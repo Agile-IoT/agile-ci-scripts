4e4979c7d4b113036732fb507ff0a71d9cf766a9
Csaba Kiraly <csaba.kiraly@gmail.com>
2017-06-15 14:04:20 +0200

    use functions

--- after_success.sh
+++ after_success.sh
@@ -1,14 +1,23 @@
 # If a docker tag and image is present let's push it to dockerhub
+function docker_push_if_needed {
 if [[ -n ${DOCKER_TAG} && ${DOCKER_IMAGE} ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
   docker push $DOCKER_IMAGE:$DOCKER_TAG;
 else
   echo "DOCKER_TAG & DOCKER_IMAGE is not set, aborting push to dockerhub"
+  return 0
 fi
+}
 
+function versionist_if_needed {
 # If is on master and doesn't have tag and it's instructed by the versionist envar then run versionist
 if [[ ${TRAVIS_PULL_REQUEST} == "false" && ${TRAVIS_BRANCH} == "master" && -z ${TRAVIS_TAG} && VERSIONIST ]]; then
   git checkout master;
   git remote set-url origin https://$GH_TOKEN@github.com/agile-iot/$COMPONENT.git;
   versionist;
 fi
+
+}
+
+docker_push_if_needed
+versionist_if_needed
