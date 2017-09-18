6382e5504165816497528da007ba3404173dc7c8
Csaba Kiraly <csaba.kiraly@gmail.com>
2017-09-18 09:51:17 +0200

    just converting to "()" from "function"

--- agile-ci-functions.sh
+++ agile-ci-functions.sh
@@ -9,7 +9,7 @@
 # Contributors:
 #     Create-Net / FBK - initial API and implementation
 #-------------------------------------------------------------------------------
-function get_docker_tag {
+get_docker_tag () {
   # if is PR set DOCKER_TAG to PR BRANCH NAME
   if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
     echo "$TRAVIS_PULL_REQUEST_BRANCH-$TRAVIS_PULL_REQUEST_SHA";
@@ -27,7 +27,7 @@ function get_docker_tag {
 
 }
 
-function bootstrap {
+bootstrap () {
   export DOCKER_TAG
   DOCKER_TAG=$(get_docker_tag)
   echo "DOCKER_TAG set as $DOCKER_TAG"
@@ -43,13 +43,13 @@ function bootstrap {
   fi
 }
 
-function docker_upgrade {
+docker_upgrade () {
   wget https://download.docker.com/linux/ubuntu/dists/trusty/pool/test/amd64/docker-ce_17.05.0~ce~rc3-0~ubuntu-trusty_amd64.deb
   sudo dpkg -i --force-confnew docker-ce_17.05.0~ce~rc3-0~ubuntu-trusty_amd64.deb
 }
 
 # build docker image
-function docker_build_if_needed {
+docker_build_if_needed () {
   if [[ -n ${DOCKER_TAG} ]]; then
     if [ "$BASEIMAGE" ] ; then sed -i "s/^FROM .*/FROM $BASEIMAGE/" Dockerfile ; fi
     if [[ -n ${BASEIMAGE_BUILD} ]]; then
@@ -64,7 +64,7 @@ function docker_build_if_needed {
 }
 
 # If a docker tag and image is present let's push it to dockerhub
-function docker_push_if_needed {
+docker_push_if_needed () {
   if [[ -n ${DOCKER_TAG} && ${DOCKER_IMAGE} ]]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
     docker push $DOCKER_IMAGE:$DOCKER_TAG;
@@ -79,7 +79,7 @@ function docker_push_if_needed {
 }
 
 # If is on master and doesn't have tag and it's instructed by the versionist envar then run versionist
-function versionist_if_needed {
+versionist_if_needed () {
   if [[ ${TRAVIS_PULL_REQUEST} == "false" && ${TRAVIS_BRANCH} == "master" && -z ${TRAVIS_TAG} && "$VERSIONIST" == "true" ]]; then
     git checkout master;
     git remote set-url origin https://$GH_TOKEN@github.com/agile-iot/$COMPONENT.git;
@@ -88,7 +88,7 @@ function versionist_if_needed {
 }
 
 # Load Travis cache file into docker images, if available
-function cache_load {
+cache_load () {
   if [ -f "$DOCKER_CACHE_FILE" ]; then
     gunzip -c $DOCKER_CACHE_FILE | docker load || true;
   fi
@@ -96,7 +96,7 @@ function cache_load {
 
 # Save docker images to Travis cache.
 # Note: This has to be called in "script", otherwise the cache is not saved by Travis
-function cache_save {
+cache_save () {
   if [[ ${TRAVIS_PULL_REQUEST} == "false" ]]; then
     mkdir -p $(dirname ${DOCKER_CACHE_FILE});
     docker save $(docker history -q $DOCKER_IMAGE:$DOCKER_TAG | grep -v '<missing>') | gzip > ${DOCKER_CACHE_FILE};
